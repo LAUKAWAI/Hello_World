a374a9701b9abf129b81eae6069506ec7ab2e98d
LAU KA WAI <17602055563@163.com>
2020-05-29 09:35:28 +0800

    实现功能：注册功能  已实现：注册保存数据逻辑  未实现：注册界面回调登录界面

--- app/src/main/java/com/example/helloworld/RegisterActivity.java
+++ app/src/main/java/com/example/helloworld/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
@@ -34,18 +35,35 @@ public class RegisterActivity extends AppCompatActivity {
                 String toast;
                 if (name.equals("")) {
                     toast = "请填写昵称";
-                }  else if (sex.equals("")) {
+                } else if (sex.equals("")) {
                     toast = "请选择性别";
                 } else if (pwd.length() != 6) {
                     toast = "密码长度必须为6位";
                 } else if (!pwd.equals(pwdOK)) {
                     toast = "请确保再次输入密码一致";
-                }else if (phone.length() != 11) {
+                } else if (phone.length() != 11) {
                     toast = "请使用正确的中国大陆手机号码";
                 } else if (!protocol) {
                     toast = "请同意本软件的用户协议，否则无法注册";
                 } else {
-                    toast = "注册成功";
+                    //存储该用户得注册数据
+                    //SharedPreferences存储技术
+                    SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
+                    String temp = sp.getString("phone_" + phone, "0");//如果是0，就说明没有找到该手机号
+                    if (!temp.equals("0")) {//如果不是0,则表示该手机号码是被已被注册
+                        toast = "该手机号码已被注册";
+                    } else {
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putString("phone_" + phone, phone);
+                        editor.putString("name_" + phone, name);
+                        editor.putString("sex_" + phone, sex);
+                        editor.putString("pwd_" + phone, pwd);
+                        temp = (sms ? "1" : "0");//判断是否接收消息推送
+                        editor.putString("sms_" + phone, name);
+                        editor.apply();
+                        toast = "注册成功";
+                        //跳转至登录界面
+                    }
                 }
                 Toast.makeText(RegisterActivity.this, toast, Toast.LENGTH_LONG).show();
 
